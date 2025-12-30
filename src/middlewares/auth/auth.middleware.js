f4fc6977c9b74a65053279fbfb8d5653f4b713ad
SuyashMehare <suyashmehare04@gmail.com>
2025-12-30 14:49:27 +0530

    Fix: user-admin route conflict

--- src/middlewares/auth/auth.middleware.js
+++ src/middlewares/auth/auth.middleware.js
@@ -41,6 +41,7 @@ const authenticate = async (req, res, next) => {
 // Role-specific middleware
 const authorizeRoles = (...roles) => {
   return (req, res, next) => {
+    console.log(roles, req.user.role)
     if (!roles.includes(req.user.role)) {
       return res.status(403).json({ 
         error: `Role ${req.user.role} is not authorized to access this resource` 
